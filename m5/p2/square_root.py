0c15295571df679f39abd0ac9e7ff16573827169
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-05 23:00:38 +0530

    " -> 0 of 3 passed. pylint: 10/10 "

--- m5/p2/square_root.py
+++ m5/p2/square_root.py
@@ -1,20 +1,19 @@
-'''# Write a python program to find the square root of the given number 
-# using approximation method'''
-
+'''# Write a python program to find the square root of the given number'''
+# using approximation method
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+# testcase 2
+# input: 49
+# output: 6.999999999999991
 def main():
-	'''Program start here'''
-	st_ = float(input())
-	"""#your code here"""
-	low_ = 0
-	high_ = st_
-	guess_ = (low_+high_)/2
-	mimimal_ = 0.01
-	while (guess_**2-st_) <= mimimal_:
-		if abs(guess_**2 - st_) == 0:
-			break
-		if guess_**2 > st_:
-			high_ = guess_
-		else:low_ = guess_
-	print(guess_)
-if __name__== "__main__":
-	main()
+    """Program starts here"""
+    sq_ = int(input())
+    epsilon_ = 0.01
+    step_ = 0.1
+    guess_ = 0.0
+    while abs(guess_**2-sq_) >= epsilon_ and (guess_ <= sq_):
+        guess_ = guess_+step_
+    print(guess_)
+if __name__ == "__main__":
+    main()
