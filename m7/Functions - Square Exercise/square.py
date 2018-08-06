564192628a2f773e041591dd754ddcb7ca02183e
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-06 10:28:15 +0530

    square1.0

--- m7/Functions - Square Exercise/square.py
+++ m7/Functions - Square Exercise/square.py
@@ -1,27 +1,17 @@
-# Exercise: square
+'''# Exercise: square
 # Write a Python function, square, that takes in one number and returns the square of that number.
-
-# This function takes in one number and returns one number.
-
-
-def square(x):
-    '''
-    x: int or float.
-    '''
-    # Your code here
-    return x*x
-    
-# Correct
-
+# This function takes in one number and returns one number.'''
+def square(xi_):
+    '''x: int or float.'''
+    return xi_*xi_
 def main():
+    '''Main program starts here'''
     data = input()
     data = float(data)
     temp = str(data).split('.')
-    if(temp[1] == '0'):
+    if temp[1] == '0':
         print(square(int(float(str(data)))))
     else:
         print(square(data))
-
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
-
