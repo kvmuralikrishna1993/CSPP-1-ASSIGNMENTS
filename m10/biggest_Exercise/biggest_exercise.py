83d76dd4592dacbe5d14b72ba346bd8fec859f27
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-09 20:37:04 +0530

    big

---
+++ m10/biggest_Exercise/biggest_exercise.py
@@ -0,0 +1,31 @@
+'''#Exercise : Biggest Exercise
+#Write a procedure, called biggest, which returns
+the key corresponding to the entry with
+the largest number of values associated with it.
+If there is more than one such entry,
+return any one of the matching keys.'''
+def biggest_(adict_):
+    '''aDict: A dictionary, where all the values are lists.
+    returns: The key with the largest number of values associated with it'''
+    key_ = list(adict_.keys())
+    val_ = list(adict_.values())
+    varlen_ = []
+    for i in val_:
+        varlen_.append(len(i))
+    index_ = varlen_.index(max(varlen_))
+    return key_[index_]
+
+def main():
+    '''Main program starts here'''
+    num_ = input()
+    adict_ = {}
+    for i in range(int(num_)):
+        set_ = input()
+        len_ = set_.split()
+        if len_[0][0] not in adict_:
+            adict_[len_[0][0]] = [len_[1]]
+        else:
+            adict_[len_[0][0]].append(len_[1])
+    print(biggest_(adict_))
+if __name__ == "__main__":
+    main()
